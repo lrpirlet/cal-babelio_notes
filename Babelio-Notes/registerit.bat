e51ae51a23ed66d5c9770c7194862504c5c7e6e3
lrpirlet <lrpirlet@gmail.com>
2023-05-28 17:28:13 +0200

    added LICENCE and command file for debug

---
+++ Babelio-Notes/registerit.bat
@@ -0,0 +1,4 @@
+rem see https://manual.calibre-ebook.com/generated/en/calibre-customize.html
+rem under *x, from a shell prompt, issue something like:  sudo calibre-customize -b . 
+
+calibre-customize -b . 
