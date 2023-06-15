f018408cb6d45156cd466ba374daeb824ee4a5e7
lrpirlet <lrpirlet@gmail.com>
2023-06-15 13:53:26 +0200

    ooops seems that I need better project management... removed junk

--- Babelio-Notes/junk/noosfere/testit.bat
+++
@@ -1,5 +0,0 @@
-rem see https://manual.calibre-ebook.com/generated/en/calibre-customize.html
-rem see https://manual.calibre-ebook.com/generated/en/calibre-debug.html
-
-calibre-customize -b .
-calibre-debug -e __init__.py
--- Babelio-Notes/junk/noosfere_util/registerit.bat
+++
@@ -1,4 +0,0 @@
-rem see https://manual.calibre-ebook.com/generated/en/calibre-customize.html
-rem under *x, from a shell prompt, issue something like:  sudo calibre-customize -b . 
-
-calibre-customize -b . 
